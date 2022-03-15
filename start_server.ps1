863e803dfe16552b8469aab4a6ad2adb3b77e2f5
Paul Travis <paul.travis@voicefoundry.com>
2022-03-15 13:04:58 -0400

    Initial Commit

---
+++ start_server.ps1
@@ -0,0 +1,31 @@
+﻿param (
+  [Parameter(Mandatory)][String]$method,
+  [Int]$port = 8000,
+  <#[ValidateScript({Test-Path $_})]#>[String]$dbPath
+)
+
+if ($method -eq 'profile') {
+
+  $method = $null
+
+} elseif ($method -eq 'inMemory' -or $method -eq 'inmemory') {
+
+  $method = "-" + $method
+  $dbPath = $null 
+
+} elseif ($method -eq 'sharedDb' -or $method -eq 'sharedb') {
+
+  $method = "-" + $method
+  $dbPath = '"-dbPath ' + $dbPath + '"'
+
+  echo $dbPath
+
+} else {
+
+  Write-host "Error occurred while parsing parameters"
+  Write-host "Method must be one of 'inMemory', 'sharedDb', or 'profile'."
+  exit
+
+}
+
+java -D"java.library.path=./DynamoDBLocal_lib" -jar DynamoDBLocal.jar $method $dbPath -port $port 
