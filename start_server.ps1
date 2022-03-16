471d3b62ae99dd6b00b104b91bf80d063715a38b
Paul Travis <paul.travis@voicefoundry.com>
2022-03-16 12:20:45 -0400

    Added doc block at top of script and updated README

--- start_server.ps1
+++ start_server.ps1
@@ -4,6 +4,37 @@
   <#[ValidateScript({Test-Path $_})]#>[String]$dbPath
 )
 
+<#
+  .SYNOPSIS
+  Used to start an instance of the DynamoDB local server
+
+  .DESCRIPTION
+  Takes a series of parameters and uses them to start an instance
+  of the local DynamoDB server. Expects the server to be located in
+  the same folder as the script.
+
+  .PARAMETER method
+  Specifies how to store the database file. Valid options are
+  profile, inMemory, and sharedDb
+
+  .PARAMETER port
+  Optional. Specifies the port to use for the server.
+
+  .PARAMETER dbPath
+  Optional. Specifies where to store the Database file.
+  Does not work when using the inMemory method.
+
+  .INPUTS
+  None.
+
+  .OUPUTS
+  None.
+
+  .EXAMPLE
+  .\start_server.ps1 -method inMemory
+
+#>
+
 if ($method -eq 'profile') {
 
   $method = $null
